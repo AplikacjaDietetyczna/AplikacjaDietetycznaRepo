d70991c26c93c07f4f6faa68de90e8176e046388
BuchyPL <62808706+BuchyPL@users.noreply.github.com>
2020-05-03 11:37:19 +0200

    Utworzenie klasy AzureSQLServerConnection

---
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/AzureSQLServerConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+namespace AplikacjaDietetyczna.Klasy
+{
+    class AzureDB
+    {
+        public static string GetConnectionStrings()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["conString"].ToString();
+            return connectionString;
+        }
+        public static string sql;
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand("", con);
+        public static SqlDataReader rd;
+        public static DataTable dt;
+        public static SqlDataAdapter da;
+        public static void openConnection()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.ConnectionString = GetConnectionStrings();
+                    con.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("wystapil blad z polaczeniem do bazy azure sql."
+                    + Environment.NewLine + "opis: " + ex.Message.ToString(), "Connect to SQL Server"
+                    , MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static void closeConnection()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+        }
+    }
+}
