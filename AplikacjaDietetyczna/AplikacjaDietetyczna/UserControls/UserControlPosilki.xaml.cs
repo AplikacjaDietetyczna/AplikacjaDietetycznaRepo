9292f7a4068a41da70a645627f2c462c9ef018d2
Pajfu <pajfot@gmail.com>
2020-05-21 02:16:15 +0200

    Dodano podliczanie dziennego zapotrzebowania dla zalogwanego Usera. Dane pokazują się w stopce

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPosilki.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPosilki.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AplikacjaDietetyczna.Klasy;
 
 namespace AplikacjaDietetyczna.UserControls
 {
@@ -30,14 +31,35 @@ namespace AplikacjaDietetyczna.UserControls
             String message = "Nie udało się wyliczyć zapotrzebowania dziennego";
             try
             {
-               
+                //Harris-Benedict
+                //Mężczyźni: 66.5 + (13.75 * waga) + (5.003 * wzrost) - (6.775 * wiek) Kobiety: 655.1 + (9.563 * waga) + (1.85 * wzrost)-(4.676 * wiek)
+                double DzienneZapotrzebowanie = 0;
+                if(FunkcjeGlobalne.Plec == "M")
+                {
+                    DzienneZapotrzebowanie = 66.5 + (13.75 * Convert.ToDouble(FunkcjeGlobalne.Waga)) + (5.003 * Convert.ToDouble(FunkcjeGlobalne.Wzrost)) - (6.775 * Convert.ToDouble(FunkcjeGlobalne.Wiek));
+                }
+                else
+                {
+                    DzienneZapotrzebowanie = 655.1 + (9.563 * Convert.ToDouble(FunkcjeGlobalne.Waga)) + (1.85 * Convert.ToDouble(FunkcjeGlobalne.Wzrost)) - (4.676 * Convert.ToDouble(FunkcjeGlobalne.Wiek));
+                }
+                //Do drugiego miejsca po przecinku
+                double Bialka = Math.Round((Double)DzienneZapotrzebowanie*0.15, 2);
+                double Kalorie = Math.Round((Double)DzienneZapotrzebowanie, 2);
+                double Tluszcze = Math.Round((Double)DzienneZapotrzebowanie * 0.30, 2);
+                double Weglowodany = Math.Round((Double)DzienneZapotrzebowanie * 0.55, 2);
+
+                TextBoxKalorie.Text = "Kalorie: Ile / " + Kalorie + " kcal";
+                TextBoxBialka.Text = "Białka: Ile / " + Bialka + " g";
+                TextBoxTluszcze.Text = "Tłuszcze: Ile / " + Tluszcze + " g";
+                TextBoxWeglowodany.Text = "Węglowodany: Ile / " + Weglowodany + " g";
+
+
             }
             catch (Exception ex)
             {
 
                 message = ex.Message.ToString();
             }
-
 
 
         }
