182b28dfa5fa526ec36d78cebd9ec6d628f8fd93
BuchyPL <62808706+BuchyPL@users.noreply.github.com>
2020-05-24 06:39:03 +0200

    usunito przycisc sql, dodano mozliwosc edycji danych do UserControlsSQL, usunieto wdrazanie danych za pomoca entera

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlSQL.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlSQL.xaml.cs
@@ -22,59 +22,50 @@ namespace AplikacjaDietetyczna.UserControls
     /// </summary>
     public partial class UserControlSQL : UserControl
     {
-        public UserControlSQL()
+        private void WypelnijTabele()
         {
-            InitializeComponent();
             AzureDB.openConnection();
             AzureDB.sql = Select.Text;
             AzureDB.cmd.CommandType = CommandType.Text;
             AzureDB.cmd.CommandText = AzureDB.sql;
+            AzureDB.cmd.ExecuteNonQuery();
             AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
             AzureDB.dt = new DataTable();
             AzureDB.da.Fill(AzureDB.dt);
             SelectDataGrid.ItemsSource = AzureDB.dt.DefaultView;
             AzureDB.closeConnection();
+        }
+        public UserControlSQL()
+        {
+            InitializeComponent();
+            WypelnijTabele();
 
         }
         private void Zatwierdz(Object sender, KeyEventArgs e)//wykonywanie query za pomoca entera
         {
+            /*
             if (e.Key == Key.Enter)
             {
                 try
                 {
-                    AzureDB.openConnection();
-                    AzureDB.sql = Select.Text;
-                    AzureDB.cmd.CommandType = CommandType.Text;
-                    AzureDB.cmd.CommandText = AzureDB.sql;
-                    AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
-                    AzureDB.dt = new DataTable();
-                    AzureDB.da.Fill(AzureDB.dt);
-                    SelectDataGrid.ItemsSource = AzureDB.dt.DefaultView;
-                    AzureDB.closeConnection();
+                    WypelnijTabele();
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Niepoprawne Query", "Edycja", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("Niepoprawne Query Error:" + ex, "Edycja", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+            */
         }
         private void Click_sql(object sender, RoutedEventArgs e)
         {
             try
             {
-                AzureDB.openConnection();
-                AzureDB.sql = Select.Text;
-                AzureDB.cmd.CommandType = CommandType.Text;
-                AzureDB.cmd.CommandText = AzureDB.sql;
-                AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
-                AzureDB.dt = new DataTable();
-                AzureDB.da.Fill(AzureDB.dt);
-                SelectDataGrid.ItemsSource = AzureDB.dt.DefaultView;
-                AzureDB.closeConnection();
+                WypelnijTabele();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                MessageBox.Show("Niepoprawne Query", "Edycja", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Niepoprawne Query Error:"+ex, "Edycja", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
