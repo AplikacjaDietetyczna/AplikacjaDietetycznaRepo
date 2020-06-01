0b01706b319a8a16a2f1842d47ba99fcc235ce23
BuchyPL <62808706+BuchyPL@users.noreply.github.com>
2020-06-01 07:47:42 +0200

    Dodanie Testu do pobierania danych z tabeli

---
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/PobieranieDanych.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AplikacjaDietetyczna.Klasy
+{
+    public class PobieranieDanych
+    {
+        public static int PobierzIdProduktu(string produkt)
+        {
+            int id=0;
+            AzureDB.openConnection();
+            AzureDB.sql = "Select * from Produkty where NazwaProduktu='"+produkt+"'";
+            AzureDB.cmd.CommandType = CommandType.Text;
+            AzureDB.cmd.CommandText = AzureDB.sql;
+            AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
+            AzureDB.dt = new DataTable();
+            AzureDB.da.Fill(AzureDB.dt);
+            if (AzureDB.dt.Rows.Count > 0)
+            {
+                id = Convert.ToInt16(AzureDB.dt.Rows[0]["ID_Produktu"]);
+
+            }
+            AzureDB.closeConnection();
+            return id;
+        }
+        public static string PobierzNazweProduktu(int id)
+        {
+            string nazwa="";
+            AzureDB.openConnection();
+            AzureDB.sql = "Select * from Produkty where ID_Produktu=" + id;
+            AzureDB.cmd.CommandType = CommandType.Text;
+            AzureDB.cmd.CommandText = AzureDB.sql;
+            AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
+            AzureDB.dt = new DataTable();
+            AzureDB.da.Fill(AzureDB.dt);
+            if (AzureDB.dt.Rows.Count > 0)
+            {
+                nazwa = AzureDB.dt.Rows[0]["NazwaProduktu"].ToString();
+
+            }
+            AzureDB.closeConnection();
+            return nazwa;
+        }
+    }
+}
