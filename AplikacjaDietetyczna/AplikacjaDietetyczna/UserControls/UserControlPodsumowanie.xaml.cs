8a35d8d9ca575491f0b735d063eea9e5f139c0cf
Sasek247 <sasek24713@gmail.com>
2020-06-03 18:08:51 +0200

    Zmiana wyglądu okna dla podsumowania

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPodsumowanie.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPodsumowanie.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using AplikacjaDietetyczna.Klasy;
+using AplikacjaDietetyczna;
 
 namespace AplikacjaDietetyczna.UserControls
 {
@@ -23,6 +27,28 @@ namespace AplikacjaDietetyczna.UserControls
         public UserControlPodsumowanie()
         {
             InitializeComponent();
+
+            string NazwaUzytkownika = FunkcjeGlobalne.Login;
+
+            AzureDB.openConnection();
+            AzureDB.sql = "SELECT Login, Plec, Wiek, Wzrost, Waga, Data FROM Users INNER JOIN Waga ON Users.ID_User=Waga.ID_User WHERE Users.Login='"+NazwaUzytkownika+"'";
+            AzureDB.cmd.CommandType = CommandType.Text;
+            AzureDB.cmd.CommandText = AzureDB.sql;
+            AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
+            AzureDB.dt = new DataTable();
+            AzureDB.da.Fill(AzureDB.dt);
+            if (AzureDB.dt.Rows.Count > 0)
+            {
+                UzytkownikLogin.Text = AzureDB.dt.Rows[0]["Login"].ToString();
+                UzytkownikPlec.Text = AzureDB.dt.Rows[0]["Plec"].ToString();
+                UzytkownikWiek.Text = AzureDB.dt.Rows[0]["Wiek"].ToString();
+                UzytkownikWzrost.Text = AzureDB.dt.Rows[0]["Wzrost"].ToString();
+                UzytkownikWaga.Text = AzureDB.dt.Rows[0]["Waga"].ToString();
+                UzytkownikDataWazenia.Text = AzureDB.dt.Rows[0]["Data"].ToString();
+            }
+            AzureDB.closeConnection();
         }
+
+        
     }
 }
