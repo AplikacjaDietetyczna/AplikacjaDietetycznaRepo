81155598267a1240a6391567289568d31224d679
Kuba <kuba.piecuch.kp@gmail.com>
2020-06-09 14:13:26 +0200

    Drobna poprawa graficzna Kalkulatora

---
+++ AplikacjaDietetyczna/AplikacjaDIetetyczna.UnitTests/UnitTest3.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using AplikacjaDIetetyczna;
+
+
+namespace AplikacjaDIetetyczna.UnitTests
+{
+    class TestyJednostkowe3
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
