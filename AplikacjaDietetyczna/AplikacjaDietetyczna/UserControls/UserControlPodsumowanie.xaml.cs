1f29ce8778c719f07c85e9d1dfa02073b0edda50
Pajfu <pajfot@gmail.com>
2020-06-12 01:27:11 +0200

    Dodano logikę za podliczaniem dziennego zapotrzebowania, reszta będzie wyglądała podobnie

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPodsumowanie.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlPodsumowanie.xaml.cs
@@ -33,9 +33,33 @@ namespace AplikacjaDietetyczna.UserControls
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-           
 
+            string sqlFormattedDate = DateKlasa.GetDate(Convert.ToInt32(FunkcjeGlobalne.Data)).ToString("yyyy-MM-dd");
 
+            String message = "Nie udało połączyć się z bazą danych"; 
+
+            try
+            {
+                AzureDB.openConnection();
+                AzureDB.sql = "SELECT (Ilosc * Kalorie) AS KalorieRazem FROM Users INNER JOIN Posilki ON Posilki.ID_User = Users.ID_User INNER JOIN PosilkiProdukty ON Posilki.ID_Posilku = PosilkiProdukty.ID_Posilku INNER JOIN Produkty ON Produkty.ID_Produktu = PosilkiProdukty.ID_Produktu WHERE Data = '"+sqlFormattedDate+"'";
+                AzureDB.cmd.CommandType = CommandType.Text;
+                AzureDB.cmd.CommandText = AzureDB.sql;
+                AzureDB.da = new SqlDataAdapter(AzureDB.cmd);
+                AzureDB.dt = new DataTable();
+                AzureDB.da.Fill(AzureDB.dt);
+                if (AzureDB.dt.Rows.Count > 0)
+                {
+                    DzienneKalorie.Text = AzureDB.dt.Rows[0]["KalorieRazem"].ToString();
+                }
+                AzureDB.closeConnection();
+            }
+            catch (Exception ex)
+            {
+
+                message = ex.Message.ToString();
+            }
+
+            DzienneKalorie.Text += "/"+Zapotrzebowanie.GetKalorie()+" kcal";
 
 
 
