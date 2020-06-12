d6388d8d98857b2325280880b644d2057b545c58
Kuba <kuba.piecuch.kp@gmail.com>
2020-06-12 18:30:08 +0200

    Dodano metode ReadFromFile() i jej logike do klasy Read.

--- AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/Read.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/Read.cs
@@ -8,6 +8,11 @@ namespace AplikacjaDietetyczna.Klasy
 {
     class Read
     {
+        public static string ReadFromFile()
+        {
+            string loginRead = System.IO.File.ReadAllText(@"C:\Users\Public\AplikacjaDietetyczna\Login.txt");
 
+            return loginRead;
+        }
     }
 }
