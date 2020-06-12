67c2344581f63547e5ddff8c1dcc4a01979e7526
Kuba <kuba.piecuch.kp@gmail.com>
2020-06-12 13:54:05 +0200

    Dodanie UnitTest3.

---
+++ AplikacjaDietetyczna/AplikacjaDIetetyczna.UnitTests/UnitTest3.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using AplikacjaDIetetyczna;
+
+namespace AplikacjaDIetetyczna.UnitTests
+{
+    class TestyJednostkowe3
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
--- AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/PobieranieDanych.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/Klasy/PobieranieDanych.cs
@@ -47,5 +47,7 @@ namespace AplikacjaDietetyczna.Klasy
             AzureDB.closeConnection();
             return nazwa;
         }
+
+
     }
 }
