d1394c7797b73dc5b3e1e62f79c9f2bb5aec061c
Sasek247 <sasek24713@gmail.com>
2020-06-16 18:26:09 +0200

    Dodanie wypisywania maila w textboxie

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlProfil.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlProfil.xaml.cs
@@ -36,7 +36,7 @@ namespace AplikacjaDietetyczna.UserControls
 
             //Połączenie z bazą i pobranie danych z zapytania
             AzureDB.openConnection();
-            AzureDB.sql = "SELECT Login, Plec, Wiek, Wzrost, Waga, Data FROM Users INNER JOIN Waga ON Users.ID_User=Waga.ID_User WHERE Users.Login='" + NazwaUzytkownika + "'";
+            AzureDB.sql = "SELECT Login, Email, Plec, Wiek, Wzrost, Waga, Data FROM Users INNER JOIN Waga ON Users.ID_User=Waga.ID_User WHERE Users.Login='" + NazwaUzytkownika + "'";
             AzureDB.cmd.CommandType = CommandType.Text;
             AzureDB.cmd.CommandText = AzureDB.sql;
             //Tworzenie tabeli tymczasowej z pobranymi danymi i zapełnienie jej tymi danymi
@@ -53,6 +53,7 @@ namespace AplikacjaDietetyczna.UserControls
                 UzytkownikWzrost.Text = AzureDB.dt.Rows[0]["Wzrost"].ToString();
                 UzytkownikWaga.Text = AzureDB.dt.Rows[0]["Waga"].ToString();
                 UzytkownikDataWazenia.Text = AzureDB.dt.Rows[0]["Data"].ToString();
+                UzytkownikEmail.Text = AzureDB.dt.Rows[0]["Email"].ToString();
             }
             DataPierwszegoWazenia();
 
