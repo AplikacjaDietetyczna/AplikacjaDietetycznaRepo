a981d5f2293263242cbdf6ab3ba86747028f9ed1
Sasek247 <sasek24713@gmail.com>
2020-06-18 17:50:13 +0200

    Dodano aktualizację wagi

--- AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlWaga.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/UserControls/UserControlWaga.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AplikacjaDietetyczna.Klasy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace AplikacjaDietetyczna.UserControls
 {
@@ -33,8 +35,19 @@ namespace AplikacjaDietetyczna.UserControls
 
         private void Click_ZmianaWaga(object sender, RoutedEventArgs e)
         {
-            UserControl add = new UserControlProfil();
-            GridMain.Children.Add(add);
+            DateTime aktualnaData = DateTime.Now;
+            string sqlFormattedDate=aktualnaData.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            //int waga = Convert.ToInt32(Waga.Text);
+            AzureDB.openConnection();
+            //AzureDB.sql="SELECT"
+            //AzureDB.openConnection();
+            AzureDB.sql = "INSERT INTO Waga (ID_User, Waga, Data) VALUES ('" + FunkcjeGlobalne.ID + "','" + TextBoxWaga.Text + "','" + aktualnaData + "')";
+            AzureDB.cmd.CommandType = CommandType.Text;
+            AzureDB.cmd.CommandText = AzureDB.sql;
+            AzureDB.cmd.ExecuteNonQuery();
+            AzureDB.closeConnection();
+            //Testowe
+            MessageBoxResult rezultat = MessageBox.Show("Dodano nową wagę dla usera: " + FunkcjeGlobalne.ID + " w dacie: " + aktualnaData + " o wartości: " + TextBoxWaga.Text + " .", "Waga", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
